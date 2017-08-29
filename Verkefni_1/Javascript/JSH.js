5bebe93426d449aecda544618b50ec7d83f790e4
Kiddiflame <kiddiflame@gmail.com>
2017-08-29 14:56:48 +0000

    Add files via upload

---
+++ Verkefni_1/Javascript/JSH.js
@@ -0,0 +1,27 @@
+//Part 1
+//document.write('<h3>' + "Hallo Heimur!" + '</h3>');
+
+//part 2
+/*var muffa;
+var kaka;
+var gos;
+muffa = "Sukkuladi muffa";
+kaka = "Osta kaka";
+gos = "Coca cola";
+var el = document.getElementById('dessert_list');
+el.textContent = muffa +', '+kaka+', '+gos;*/
+
+//part 3
+/*var budarlisti; 
+budarlisti = ['braud','alegg','avextir','morgunkorn','mjolk'];
+
+var listi; 
+listi = '';
+
+for (i = 0; i < budarlisti.length; i++)
+{
+listi += budarlisti[i]+ ' ';
+}
+
+var el = document.getElementById('listi');
+el.textContent = listi;*/
